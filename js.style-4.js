4f5c4cb0c88d4e18de4b3813a1172f9f94214e56
z-chubinidze <120126922+z-chubinidze@users.noreply.github.com>
2023-05-06 12:49:16 +0400

    Add files via upload

---
+++ js.style-4.js
@@ -0,0 +1,17 @@
+function mwvane(){
+   
+    document.body.style.backgroundColor = "green";
+    document.getElementById("borderi").style.backgroundColor="blue";
+    document.getElementById("borderi").style.borderColor="yellow";
+    document.getElementById("gilaki1").style.color="black";
+}
+mwvane()
+
+function lurji(){
+   
+    document.body.style.backgroundColor = "blue";
+    document.getElementById("borderi").style.backgroundColor="green";
+    document.getElementById("borderi").style.borderColor="red";
+    document.getElementById("gilaki2").style.color="red";
+}
+lurji()
